fb123618655b84c431da9535a1a9414039af8f82
SebSusi <sebastien@boleromusic.com>
2023-07-05 10:59:42 +0200

    try to resolve 403

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET!,
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
